00b2e5774c4423ef8c3d4576d6e88c0ba31224ed
coreminw <coreminw@naver.com>
2022-06-27 03:10:10 +0900

    Add: password 수정 후 MailSend 와 관련된 코드 구현

--- src/main/java/capstone/sangcom/service/user/EmailServiceImpl.java
+++ src/main/java/capstone/sangcom/service/user/EmailServiceImpl.java
@@ -1,8 +1,13 @@
 package capstone.sangcom.service.user;
 
+import capstone.sangcom.dto.login.FindPasswordDTO;
 import capstone.sangcom.dto.login.MailDTO;
+import capstone.sangcom.entity.User;
+import capstone.sangcom.repository.user.UserRepository;
+import org.apache.tomcat.util.net.openssl.ciphers.Encryption;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 
@@ -10,7 +15,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class EmailServiceImpl implements EmailService{
 
+    private final UserRepository userRepository;
     private JavaMailSender emailSender;
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void sendFeedback(MailDTO mailDTO) {
@@ -20,5 +27,48 @@ public class EmailServiceImpl implements EmailService{
         message.setSubject(mailDTO.getTitle());
         message.setText(mailDTO.getContent());
         emailSender.send(message);
+    }
+
+    @Override
+    public MailDTO createMailAndChangePassword(User user) {
+        String str = getTempPassword();
+        MailDTO dto = new MailDTO();
+        dto.setAddress(user.getEmail());
+        dto.setTitle(user.getName()+"님의 임시비밀번호 안내 이메일 입니다.");
+        dto.setContent("안녕하세요. HOTTHINK 임시비밀번호 안내 관련 이메일 입니다." + "[" + user.getName() + "]" +"님의 임시 비밀번호는 "
+                + str + " 입니다.");
+        updatePassword(str,user);
+        return dto;
+    }
+
+    @Override
+    public void mailSend(MailDTO mailDTO) {
+        System.out.println("이멜 전송 완료!");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailDTO.getAddress());
+        message.setSubject(mailDTO.getTitle());
+        message.setText(mailDTO.getContent());
+
+        emailSender.send(message);
+    }
+
+    public void updatePassword(String str, User user){
+        String pw = passwordEncoder.encode(str);
+        String id = userRepository.findById(user.getEmail()).getId();
+        userRepository.update(id,pw);
+    }
+
+    public String getTempPassword(){
+        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        String str = "";
+
+        int idx = 0;
+        for (int i = 0; i < 10; i++) {
+            idx = (int) (charSet.length * Math.random());
+            str += charSet[idx];
+        }
+        return str;
     }
 }
