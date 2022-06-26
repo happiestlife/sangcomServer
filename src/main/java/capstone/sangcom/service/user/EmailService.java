1b4735e7aa26eca405a25418878b291435994d32
coreminw <coreminw@naver.com>
2022-06-27 03:09:37 +0900

    Add: password 수정 후 MailSend 와 관련된 코드 추가

--- src/main/java/capstone/sangcom/service/user/EmailService.java
+++ src/main/java/capstone/sangcom/service/user/EmailService.java
@@ -1,7 +1,11 @@
 package capstone.sangcom.service.user;
 
+import capstone.sangcom.dto.login.FindPasswordDTO;
 import capstone.sangcom.dto.login.MailDTO;
+import capstone.sangcom.entity.User;
 
 public interface EmailService {
     public void sendFeedback(MailDTO mailDTO);
+    public MailDTO createMailAndChangePassword(User user);
+    public void mailSend(MailDTO mailDTO);
 }
