0456a1c795d2b572567904cababfa48656c2141a
JAE <jack3787@naver.com>
2022-06-30 17:11:53 +0900

    add : annotation 설명 추가

--- src/main/java/capstone/sangcom/dto/login/FindPasswordDTO.java
+++ src/main/java/capstone/sangcom/dto/login/FindPasswordDTO.java
@@ -3,6 +3,8 @@ package capstone.sangcom.dto.login;
 
 import lombok.Data;
 
+// @Data는 callSuper, includeFieldName, exclude와 같은 파라미터와 같이 사용될 수 없다.
+// @Data = @Getter/Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
 @Data
 public class FindPasswordDTO {
 
--- src/main/java/capstone/sangcom/dto/login/LoginDTO.java
+++ src/main/java/capstone/sangcom/dto/login/LoginDTO.java
@@ -2,11 +2,11 @@ package capstone.sangcom.dto.login;
 
 import lombok.*;
 
-@Getter
-@Setter
+@Getter // 인스턴스 변수를 반환한다.
+@Setter // 인스턴스 변수를 대입하거나 수정한다.
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString // 객체가 가지고 있는 정보나 값들을 문자열로 만들어 리턴한다.
 public class LoginDTO {
     private String id;
     private String password;
