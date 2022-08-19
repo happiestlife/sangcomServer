fd3abb6e4029bafeedc668d6b8a4e0b56f52b4b9
JAE <jack3787@naver.com>
2022-08-19 15:24:56 +0900

    feat : todolist 관련 DTO

--- src/main/java/capstone/sangcom/entity/dto/todoSection/InsertTodoListDTO.java
+++ src/main/java/capstone/sangcom/entity/dto/todoSection/InsertTodoListDTO.java
@@ -3,14 +3,10 @@ package capstone.sangcom.entity.dto.todoSection;
 import lombok.Data;
 
 @Data
-public class TodoDTO {
+public class InsertTodoListDTO {
 
-    private final String user_id;
-    private final int list_id;
     private final String body;
-    private final int listCheck;
     private final int year;
     private final int month;
     private final int day;
-
 }
---
+++ src/main/java/capstone/sangcom/entity/dto/todoSection/UpdateTodoDTO.java
@@ -0,0 +1,10 @@
+package capstone.sangcom.entity.dto.todoSection;
+
+import lombok.Data;
+
+
+@Data
+public class UpdateTodoDTO {
+
+    private final String body;
+}
