5715672fb25c45753f407311ae022ad4802eeb67
JAE <jack3787@naver.com>
2022-08-19 15:26:08 +0900

    feat : todolist 레포지토리

--- src/main/java/capstone/sangcom/repository/todo/TodoRepository.java
+++ src/main/java/capstone/sangcom/repository/todo/TodoRepository.java
@@ -1,11 +1,19 @@
 package capstone.sangcom.repository.todo;
 
-import capstone.sangcom.entity.dto.todoSection.TodoEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
+import capstone.sangcom.entity.dto.todoSection.TodoDTO;
 import org.springframework.stereotype.Repository;
 
-// JpaRepository<데이터베이스와 연결될 객체, 해당 객체의 ID의 필드 타입>
+import java.util.List;
+
+
 @Repository
 public interface TodoRepository {
+
+    public int insert(TodoDTO todoDTO);
+    public List<TodoDTO> getTodoList(String user_id, TodoDTO todoDTO);
+    public List<TodoDTO> searchTodoList(String user_id, int list_id);
+    public boolean updateTodoList(String body, String user_id, int list_id);
+    public boolean deleteTodoList(String user_id, int list_id);
+    public boolean checkTodoList(boolean listCheck, String user_id, int list_id);
 
 }
