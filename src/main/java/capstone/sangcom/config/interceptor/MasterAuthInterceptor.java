6ad90c4ce02c7cec8d3692320bafef8184d6d3e6
chickenman <chickenman10@naver.com>
2022-08-19 21:29:49 +0900

    Update : 기능에 따른 설정 클래스의 분리 및 버그 수정

--- src/main/java/capstone/sangcom/config/interceptor/MasterAuthInterceptor.java
+++ src/main/java/capstone/sangcom/config/interceptor/MasterAuthInterceptor.java
@@ -1,7 +1,9 @@
 package capstone.sangcom.config.interceptor;
 
+import capstone.sangcom.controller.api.response.common.SimpleResponse;
 import capstone.sangcom.entity.JwtUser;
 import capstone.sangcom.entity.UserRole;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +15,11 @@ public class MasterAuthInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         JwtUser user = (JwtUser) request.getAttribute("user");
         if(!user.getRole().equals(UserRole.ADMIN)){
-            FailHeader.setResponse(response);
+            ObjectMapper mapper = new ObjectMapper();
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(mapper.writeValueAsString(new SimpleResponse(false)));
 
             return false;
         }
