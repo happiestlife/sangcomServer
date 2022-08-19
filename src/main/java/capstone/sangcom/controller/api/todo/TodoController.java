b0ce65a459e406777e0b08f2000210acbaa5d489
JAE <jack3787@naver.com>
2022-08-19 15:23:15 +0900

    feat : TodoController - TodoList 컨트롤러

--- src/main/java/capstone/sangcom/controller/api/todo/TodoController.java
+++ src/main/java/capstone/sangcom/controller/api/todo/TodoController.java
@@ -1,12 +1,20 @@
 package capstone.sangcom.controller.api.todo;
 
+import capstone.sangcom.controller.api.response.common.SimpleResponse;
+import capstone.sangcom.entity.JwtUser;
+import capstone.sangcom.entity.dto.todoSection.InsertTodoListDTO;
 import capstone.sangcom.entity.dto.todoSection.TodoDTO;
 import capstone.sangcom.controller.api.response.todo.TodoResponse;
+import capstone.sangcom.entity.dto.todoSection.UpdateTodoDTO;
 import capstone.sangcom.service.todo.TodoService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @CrossOrigin
 @AllArgsConstructor
@@ -23,29 +31,63 @@ public class TodoController {
     @GetMapping("?year={{year}}&month={{monthj}}&day={{day}}")
     public ResponseEntity<TodoResponse> getTodolist(){
 
+        List<TodoDTO> todo = todoService.getTodolist();
+
+        return ResponseEntity
+                .ok(new TodoResponse(true, todo));
     }
 
     /**
      * todo-list 할 일 등록
      */
     @PostMapping("")
-    public ResponseEntity<TodoDTO> insertTodoList(){
+    public ResponseEntity<SimpleResponse> insertTodoList(HttpServletRequest request,
+                                                  @RequestParam  String type, InsertTodoListDTO insertData){
 
+        JwtUser user = (JwtUser) request.getAttribute("user");
+
+        if (todoService.insert(user.getId(), type, insertData))
+            return ResponseEntity
+                    .ok(new SimpleResponse(true));
+        else
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new SimpleResponse(false));
     }
 
     /**
      * todo-list 할 일 수정
      */
     @PutMapping("/:list_id")
-    public ResponseEntity<TodoDTO> updateTodoList(){
+    public ResponseEntity<SimpleResponse> updateTodoList(HttpServletRequest request,
+                                                  @PathVariable int todoId,
+                                                  UpdateTodoDTO updataData){
+        JwtUser user = (JwtUser) request.getAttribute("user");
 
+        if (todoService.update(user.getId(), todoId, updataData))
+            return ResponseEntity
+                    .ok(new SimpleResponse(true));
+        else
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new SimpleResponse(false));
     }
 
     /**
      * todo-list 할 일 삭제
      */
     @DeleteMapping("/:list_id")
-    public ResponseEntity<TodoDTO> deleteTodoList(){
+    public ResponseEntity<SimpleResponse> deleteTodoList(HttpServletRequest request,
+                                                         @PathVariable int todoId){
+        JwtUser user = (JwtUser) request.getAttribute("user");
+
+        if(todoService.delete())
+            return ResponseEntity
+                    .ok(new SimpleResponse(true));
+        else
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new SimpleResponse(false));
 
     }
 
@@ -53,7 +95,17 @@ public class TodoController {
      * todo-list 할 일 체크
      */
     @GetMapping("/:list_id")
-    public ResponseEntity<TodoDTO> checkTodoList(){
+    public ResponseEntity<SimpleResponse> checkTodoList(HttpServletRequest request,
+                                                 @PathVariable int todoId){
+        JwtUser user = (JwtUser) request.getAttribute("user");
+
+        if(todoService.check())
+            return ResponseEntity
+                    .ok(new SimpleResponse(true));
+        else
+            return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new SimpleResponse(false));
 
     }
 
