90c945e7ca94799f602fd4d1a7099de642f7d46c
JAE <jack3787@naver.com>
2022-08-19 15:29:49 +0900

    feat : MealsParameterDTO, MealsResponseDTO - 급식 받아오기 관련 DTO (수정 필요)

---
+++ src/main/java/capstone/sangcom/entity/dto/mealsSection/MealsParameterDTO.java
@@ -0,0 +1,16 @@
+package capstone.sangcom.entity.dto.mealsSection;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class MealsParameterDTO {
+
+    private String year;
+    private String month;
+    private String day;
+    private ArrayList<String> dish;
+    private String cal_info;
+
+}
