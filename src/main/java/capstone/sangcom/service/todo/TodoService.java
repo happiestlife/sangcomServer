bf7a028a6a0ae2d213d14305b23b783a843c689e
JAE <jack3787@naver.com>
2022-08-19 15:28:13 +0900

    feat : TodoService, TodoServiceImpl - todolist 관련 서비스 틀 잡기

--- src/main/java/capstone/sangcom/service/todo/TodoService.java
+++ src/main/java/capstone/sangcom/service/todo/TodoService.java
@@ -1,13 +1,34 @@
 package capstone.sangcom.service.todo;
 
+import capstone.sangcom.entity.dto.todoSection.InsertTodoListDTO;
 import capstone.sangcom.entity.dto.todoSection.TodoDTO;
+import capstone.sangcom.entity.dto.todoSection.UpdateTodoDTO;
 
 import java.util.List;
 
 public interface TodoService {
     public List<TodoDTO> getTodolist();
 
-    public List<TodoDTO> insertTodoList();
+    public boolean insert(String userId, String type, InsertTodoListDTO insertData);
 
-    public L
+    public boolean update(String userId, int todoId, UpdateTodoDTO updataData);
+
+    public boolean delete();
+
+    public boolean check();
+//    public List<TodoDTO> insertTodoList(String body, int year, int month, int day);
+
+//    public List<>
 }
+
+
+/**
+ * 구현해야하는 메소드들 by SocialProject
+ * insertTodoList
+ * getTodoList
+ * updateTodoList
+ * deleteTodoList
+ * checkTodoList
+ *
+ * user_id, body, check, year, month, day
+ */
--- src/main/java/capstone/sangcom/service/todo/TodoServiceImpl.java
+++ src/main/java/capstone/sangcom/service/todo/TodoServiceImpl.java
@@ -1,4 +1,56 @@
 package capstone.sangcom.service.todo;
 
-public class TodoServiceImpl {
+import capstone.sangcom.entity.dto.todoSection.InsertTodoListDTO;
+import capstone.sangcom.entity.dto.todoSection.TodoDTO;
+import capstone.sangcom.entity.dto.todoSection.UpdateTodoDTO;
+import capstone.sangcom.repository.todo.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
+
+    private final TodoRepository todoRepository;
+
+
+    @Override
+    @Transactional
+    public List<TodoDTO> getTodolist() {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public boolean insert(String userId, String type, InsertTodoListDTO insertData) {
+
+
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public boolean update(String userId, int todoId, UpdateTodoDTO updataData) {
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public boolean check() {
+        return false;
+    }
 }
