65f126b5c169fb1c04a024bfb01325d32336245a
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-09-12 12:19:05 +0900

    Revert "Revert "update:글쓰기 부분 css수정""
    This reverts commit 6b255275ab6a5d670298f315fbdefc6c58ab9595.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -54,71 +54,106 @@ function changeColor() {
   bodyTag[0].style.backgroundColor = color[num];
 }
 
-function idConfirm() {
-  var BASEURL = "http://localhost:8080/api/user/confirm/name";
-  var body = {
-      id : $('#inputID').val()
-  }
-  console.log(body);
+// function idConfirm() {
+//   var BASEURL = "http://localhost:8080/api/user/confirm/name";
+//   var body = {
+//       id : $('#inputID').val()
+//   }
+//   // console.log(body);
+
+//   $.ajax({
+//       type : 'POST',
+//       url : BASEURL,
+//       data : JSON.stringify(body),
+//       datatype: "JSON",
+//       contentType: "application/json; charset=utf-8",
+//       success : function(data) {
+//         alert("성공")
+//           if(data.success == true) {
+//               // $('#idConfirm').text(true).hide();
+//               // console.log($('#idConfirm').val())
+//               console.log(JSON.stringify(data));
+//           }
+//           else{
+//               $('#checkId').text("중복된 아이디 입니다. 다른 아이디를 입력해주세요.").show()
+//                   .css('color', 'red').css('font-weight', 'bold');
+//           }
+//       },
+//       error : function() {
+//         alert("실패");
+//       }
+//   });
+// }
+
+// function studentIdConfirm() {
+//   var userYear = $('#userYear').val();
+//   // $("#셀렉트박스ID option:selected").val();
+//   var userNum = $('#schoolnumber').val();
+//   var userGrade = $('#schoolgrade option:selected').val();
+//   var userClass = $('#schoolclass option:selected').val();
+//   userClass="0"+userClass; //2022 + 1 + 01 + 01   (입학년도 + 학년 + 반 + 번호 반이랑 번호에는 0붙여서 입력)
+//   var studentID = userYear+userGrade+userClass+userNum;
+//   var BASEURL = "http://localhost:8080/api/user/auth/student/check";
+//   var body = {
+//       name : $('#userName').val(),
+//       studentId : studentID
+//   }
+//   // console.log(body);
+
+//   $.ajax({
+//       type : 'POST',
+//       url : BASEURL,
+//       data : JSON.stringify(body),
+//       datatype: "JSON",
+//       contentType: "application/json; charset=utf-8",
+//       success : function(data) {
+//         alert("성공")
+//           if(data.success == true) {
+//               // $('#idConfirm').text(true).hide();
+//               console.log($('#idConfirm').val())
+//               console.log(JSON.stringify(data));
+//           }
+//           else{
+//               alert('학번인증에 실패하였습니다.');
+//           }
+//       },
+//       error : function() {
+//         alert("실패");
+//       }
+//   });
+// }
+
+
+function userConfig() {
+
+  var BASEURL = "http://localhost:8080/api/user/info";
+  var userNick = $('#nickname').val();
+  // var body = {
+  //     name : $('#userName').val(),
+  //     studentId : studentID
+  // }
+  // console.log(body);
 
   $.ajax({
-      type : 'POST',
+      type : 'GET',
       url : BASEURL,
-      data : JSON.stringify(body),
-      datatype: "JSON",
+      // data : JSON.stringify(body),
+      // datatype: "JSON",
       contentType: "application/json; charset=utf-8",
       success : function(data) {
         alert("성공")
           if(data.success == true) {
-              // $('#idConfirm').text(true).hide();
-              // console.log($('#idConfirm').val())
+            console.log(JSON.stringify(userNick));
               console.log(JSON.stringify(data));
           }
           else{
-              $('#checkId').text("중복된 아이디 입니다. 다른 아이디를 입력해주세요.").show()
-                  .css('color', 'red').css('font-weight', 'bold');
+              alert('회원정보확인에 실패하였습니다.');
           }
       },
       error : function() {
-        alert("실패");
+        alert("회원정보 확인 실패");
       }
   });
 }
 
-function studentIdConfirm() {
-  var userYear = $('#userYear').val();
-  // $("#셀렉트박스ID option:selected").val();
-  var userNum = $('#schoolnumber').val();
-  var userGrade = $('#schoolgrade option:selected').val();
-  var userClass = $('#schoolclass option:selected').val();
-  userClass="0"+userClass; //2022 + 1 + 01 + 01   (입학년도 + 학년 + 반 + 번호 반이랑 번호에는 0붙여서 입력)
-  var studentID = userYear+userGrade+userClass+userNum;
-  var BASEURL = "http://localhost:8080/api/user/auth/student/check";
-  var body = {
-      name : $('#userName').val(),
-      studentId : studentID
-  }
-  console.log(body);
-
-  $.ajax({
-      type : 'POST',
-      url : BASEURL,
-      data : JSON.stringify(body),
-      datatype: "JSON",
-      contentType: "application/json; charset=utf-8",
-      success : function(data) {
-        alert("성공")
-          if(data.success == true) {
-              // $('#idConfirm').text(true).hide();
-              // console.log($('#idConfirm').val())
-              console.log(JSON.stringify(data));
-          }
-          else{
-              alert('학번인증에 실패하였습니다.');
-          }
-      },
-      error : function() {
-        alert("실패");
-      }
-  });
-}
+// userConfig();
