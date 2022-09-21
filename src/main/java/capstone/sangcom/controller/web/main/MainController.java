2f2bd6b8710e4790d508d51c2fed8adeec9d35cd
chickenman <chickenman10@naver.com>
2022-09-21 23:45:21 +0900

    New : 메인 페이지에 최신 게시글이 보일 수 있는 기능 추가

--- src/main/java/capstone/sangcom/controller/web/main/MainController.java
+++ src/main/java/capstone/sangcom/controller/web/main/MainController.java
@@ -1,7 +1,9 @@
 package capstone.sangcom.controller.web.main;
 
 import capstone.sangcom.entity.JwtUser;
+import capstone.sangcom.entity.dto.boardSection.BoardDTO;
 import capstone.sangcom.entity.dto.todoSection.GetTodolistResponseDTO;
+import capstone.sangcom.service.board.BoardService;
 import capstone.sangcom.service.todo.TodoService;
 import capstone.sangcom.service.user.UserService;
 import capstone.sangcom.util.image.ImageUtils;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -26,10 +29,13 @@ public class MainController {
 
     private final TodoService todoService;
 
+    private final BoardService boardService;
+
     @GetMapping
     public String getMainPage(HttpServletRequest request,
                               @CookieValue String acc,
                               Model model) {
+        // user의 개인정보
         JwtUser user = (JwtUser) request.getAttribute("user");
 
         String profilePath = imageUtils.getPath(userService.showImage(user.getId()));
@@ -40,11 +46,30 @@ public class MainController {
 
         model.addAttribute("profilePath", profilePath);
 
+        // user의 to-do 리스트
         Calendar c = Calendar.getInstance();
         List<GetTodolistResponseDTO> todolist =
                 todoService.getTodolist(user.getId(), c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DATE));
 
         model.addAttribute("todoList", todolist);
+
+        // 자유게시글과 학생회 공지글
+        List<BoardDTO> free = boardService.readAll("free");
+        List<BoardDTO> council = boardService.readAll("council");
+
+        ArrayList<BoardDTO> freePosts = new ArrayList<>();
+        ArrayList<BoardDTO> councilPosts = new ArrayList<>();
+        for (int i = 0; i < free.size(); i++) {
+            freePosts.add(free.get(i));
+            if(i < council.size())
+                councilPosts.add(council.get(i));
+            if(i == 4)
+                break;
+        }
+
+        model.addAttribute("freePosts", freePosts);
+
+        model.addAttribute("councilPosts", councilPosts);
 
         return "main";
     }
